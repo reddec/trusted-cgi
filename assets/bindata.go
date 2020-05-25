188a0c5da9e1accfa6a12c88cc7da99ef3562e96
Alexander Baryshnikov <dev@baryshnikov.net>
2020-05-25 21:29:40 +0800

    ui: fix invisible playground results

--- assets/bindata.go
+++ assets/bindata.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:510ec86a4e98f2bb527a841bf9df6f3d4cc5c92e15a0ae384d9bad9af68757d7
-size 4822550
+oid sha256:b36ed7d3712c361b916862a88d1e4f9ad42925b0d9595ce1a24d79e11a7873cf
+size 4822554

00afec32be9ea6edc22cfe10ed7883fa86b7fe0a
Alexander Baryshnikov <dev@baryshnikov.net>
2020-05-29 22:02:55 +0800

    UI UX fixes

--- assets/bindata.go
+++ assets/bindata.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3a9cdf56405262e24c6f3cddcf44b0fa5d8db14acc85eaf40978c5846756435
-size 4848219
+oid sha256:e9b580aafd5a47d0bad7558b237135df022aac7c952c854de05c55e547560824
+size 4848597

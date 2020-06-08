871d0a7df21f33c8541bab4f11bed193bc163473
Alexander Baryshnikov <dev@baryshnikov.net>
2020-06-08 20:26:01 +0800

    ui: fix show global settings user

--- assets/bindata.go
+++ assets/bindata.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8db756703e2380a5fc5652c44e64a0d8be071843e9d65c5c5400e20f2f923e9f
-size 4848967
+oid sha256:e07b096a9b6b906b5e7e8e7c85b5aa2ec67c5de2e7788cf88a269ec1558b0392
+size 4848983

eec1e28e376d8528a4b66f621381ea505aff4b64
Alexander Baryshnikov <dev@baryshnikov.net>
2020-06-17 17:08:46 +0800

    ui: allow to create from default template, add hint about cgi-ctl create command

--- assets/bindata.go
+++ assets/bindata.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ae8303365595bf340788213d50e4d60c33f5ecdc8312f691d6a1b3a4be41bf5
-size 4852375
+oid sha256:4d601ba2777569f2dd6cfcaf9a0d4bd03df8754c3a4127c0b702bbbff420cadf
+size 4853275

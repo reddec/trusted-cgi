395152fb0329c5dfd24cabf552d2091e25fc7fa0
Alexander Baryshnikov <dev@baryshnikov.net>
2020-07-25 12:09:12 +0800

    ui: update to latest version

--- assets/bindata.go
+++ assets/bindata.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a85dd93aeb9ade6027d377a005e471b5ef448d0d9cec04727d28b0caf62b5ca
-size 4902919
+oid sha256:76d99b416ad6b7b32334fe0999210d51fbf8eb63bd88ef47402937f3fdecccaf
+size 4903035

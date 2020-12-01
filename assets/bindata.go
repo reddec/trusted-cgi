5844692dbab264be777c7cf36650d23609688b38
Alexander Baryshnikov <dev@baryshnikov.net>
2020-12-01 20:58:59 +0800

    update ui, fix bug with tokens

--- assets/bindata.go
+++ assets/bindata.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76d99b416ad6b7b32334fe0999210d51fbf8eb63bd88ef47402937f3fdecccaf
-size 4903035
+oid sha256:5b1e377c5bdb294bf9b003f1e975293b8fd61cda4343fe6af31830dae670332a
+size 4907235
